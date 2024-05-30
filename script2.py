b1f8885e81e23a31e5bdd9b3bdee4177f39f7d8d
ppatel46 <ppatel46@flemingcollege.ca>
2024-05-30 19:05:59 -0400

    modified both files in the repo done
    -deleted a print() statement from script1.py
    -added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 this is good programing advice 
-print("beautiful is better than ugly")
-print("readability counts")
+print("beautiful is better than ugly.")
+print("readability counts.")
+print("now is better than never.")
