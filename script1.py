fe432893b09cf9b87696d318a5c452fa18ad925a
ppatel46 <ppatel46@flemingcollege.ca>
2025-01-14 13:24:20 -0500

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,4 +2,4 @@ print ("simple is better than complex")
 print("explicit is better than implicit.")
 print("sparse is better than dense")
 print("flat is better than nested")
-this is an amazing new feature .
+
